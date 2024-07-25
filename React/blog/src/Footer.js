5d3b8bb79c80b0514a2357030b226e99052c8a68
Nataliia Shobotova <samulikn@gmail.com>
2024-07-25 12:11:59 +0200

    used Redux

--- React/blog/src/Footer.js
+++ React/blog/src/Footer.js
@@ -1,10 +1,10 @@
-import React from "react";
+import { useStoreState } from "easy-peasy";
 
 function Footer() {
-  const today = new Date();
+  const postCount = useStoreState((state) => state.postCount);
   return (
     <footer className="Footer">
-      <p>Copyright &copy; {today.getFullYear()}</p>
+      <p>{postCount} Post page</p>
     </footer>
   );
 }
